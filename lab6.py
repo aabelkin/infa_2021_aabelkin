8a7c433e043bee2a02b87ada901cc55c7e4108aa
Belkin Andrey <belkin.aa@phystech.edu>
2021-10-08 16:03:11 +0300

    Add lab6.py

---
+++ lab6.py
@@ -0,0 +1,48 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+FPS = 2
+screen = pygame.display.set_mode((1200, 900))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+score = 0
+
+def new_ball():
+    global x, y, r
+    x = randint(100, 700)
+    y = randint(100, 500)
+    r = randint(30, 50)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+def click(event):
+    print(x, y, r)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse = event.pos[0]
+            y_mouse = event.pos[1]
+            if (x - x_mouse) ** 2 + (y - y_mouse) ** 2 <= r ** 2:
+                score += 1
+    new_ball()
+    pygame.display.update()
+    screen.fill(BLACK)
+
+pygame.quit()
