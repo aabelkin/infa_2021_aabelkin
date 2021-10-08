3a97cacb4f2db6708179d44e761a1adfdaafab87
Belkin Andrey <belkin.aa@phystech.edu>
2021-10-08 17:03:02 +0300

    Refactoring in lab6.py

--- lab6.py
+++ lab6.py
@@ -4,8 +4,10 @@ from random import randint
 import math
 pygame.init()
 
+screen_width = 1200
+screen_height = 900
 FPS = 30
-screen = pygame.display.set_mode((1200, 900))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -30,7 +32,7 @@ for ball in range(n_balls):
     angle = randint(0, 360)
     speed = randint(5, 10)
     color = COLORS[randint(0, 5)]
-    balls += [[x, y, angle, speed, color]]
+    balls += [[x, y, r, angle, speed, color]]
 
 def new_ball():
     global x, y, r
@@ -40,11 +42,11 @@ def new_ball():
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
-def draw_ball(x, y, angle, speed, color):
+def draw_ball(x, y, r, angle, speed, color):
     x += speed * math.cos(angle)
     y += speed * math.sin(angle)
     circle(screen, color, (x, y), r)
-    return (x, y)
+    return (x, y, r)
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -61,7 +63,12 @@ while not finished:
             if (x - x_mouse) ** 2 + (y - y_mouse) ** 2 <= r ** 2:
                 score += 1
     for i in range(n_balls):
-        balls[i][0:2] = draw_ball(*balls[i])
+        x, y, r = draw_ball(*balls[i])
+        balls[i][0:3] = x, y, r
+        if x - r <= 0 or x + r >= screen_width:
+            pass
+        if y - r <= 0 or y + r >= screen_height:
+            pass
     pygame.display.update()
     screen.fill(BLACK)
 
